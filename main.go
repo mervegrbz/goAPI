20d1c09af6e70f06b1f06216edda8e21e4f85531
mervegrbz <merve16gurbuz@gmail.com>
2022-08-02 17:10:08 +0300

    shitty code but I need for testing quickly

--- main.go
+++ main.go
@@ -1,31 +1,64 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
 
-    "github.com/gin-gonic/gin"
-) 
+
+
+func getIP(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK,getip2())
+	// fmt.Print(GetOutboundIP())
+	
+}
+
+type IP struct {
+    Query string 
+}
+type Ip struct {
+	IP string `json:"ip"`
+}
+func getip2() string {
+    req, err := http.Get("http://ip-api.com/json/")
+    if err != nil {
+        return err.Error()
+    }
+    defer req.Body.Close()
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        return err.Error()
+    }
+    var ip IP
+    json.Unmarshal(body, &ip)
+    return ip.Query
+}
 
 type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-
-    router.Run("localhost:8080")
+	router := gin.Default()
+	fmt.Print(getip2())
+	
+	router.GET("/albums", getAlbums)
+	router.GET("/ip",getIP)
+	router.Run(":8080")
 }
